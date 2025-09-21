37c65738971c72bb8ad947df6c70281beddfe6a9
FrancBarr <64182282+aPoncho@users.noreply.github.com>
2025-09-20 21:04:03 -0300

    Agregadas funcionalidad a la barra de navegacion principal

--- app/src/main/java/com/example/gameboxpv2/MainActivity.kt
+++ app/src/main/java/com/example/gameboxpv2/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.example.gameboxpv2.HomeFragment
+import com.example.gameboxpv2.PopularFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +20,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
+        fun replaceFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
 
         if (savedInstanceState == null) {
-            val username = intent.getStringExtra("USERNAME")
+            val username = intent.getStringExtra("USERNAME") ?: "Usuario Invitado"
             Log.d("DATA_TRACE", "MainActivity recibió: $username")
 
             val homeFragment = HomeFragment()
@@ -31,8 +45,19 @@ class MainActivity : AppCompatActivity() {
             homeFragment.arguments = bundle
 
             supportFragmentManager.beginTransaction()
-                .add(R.id.home_fragment_container, homeFragment)
+                .add(R.id.fragment_container, homeFragment)
                 .commit()
+        }
+
+        //cuando se toca un item del navigationbar
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_popular -> replaceFragment(PopularFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                else -> false
+            }
+            true
         }
     }
 }
