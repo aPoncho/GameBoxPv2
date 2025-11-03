6ce927394f8112b74ace16a90a7043a507bcedeb
diego <diegosoto2198@gmail.com>
2025-11-03 20:05:57 -0300

    local storage shared preferences

--- app/src/main/java/com/example/gameboxpv2/StatisticsFragment.kt
+++ app/src/main/java/com/example/gameboxpv2/StatisticsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.gameboxpv2
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 private const val ARG_PARAM1 = "param1"
@@ -33,6 +35,21 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val prefs = requireActivity().getSharedPreferences(
+            SettingsFragment.PREFS_NAME,
+            Context.MODE_PRIVATE
+        )
+
+        val platformValue = prefs.getString(SettingsFragment.KEY_PLATFORM, "PC")
+
+        val platformTextView = view.findViewById(R.id.tv_platform_value) as TextView
+
+        platformTextView.text = platformValue ?: "PC"
     }
 
     companion object {
